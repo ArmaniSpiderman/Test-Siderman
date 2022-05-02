19eb6520d63c405250dbd57fd5696f60987db9c0
LINZLEY MASTERS <linzleymasters@LINZLEYs-Air.home>
2022-05-02 18:04:30 +0200

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SIDERMAN
+//
+//  Created by LINZLEY MASTERS on 2022/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
